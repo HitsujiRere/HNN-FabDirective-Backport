179031b8d1806ee34435a163ed65d8ae2e0fa80b
HitsujiRere <55770125+HitsujiRere@users.noreply.github.com>
2025-12-29 17:50:19 +0900

    feat: fab_directiveの表示機能ClientAccessの仮実装

--- src/main/java/io/github/hitsujirere/hnn_fabdirective/item/FabDirectiveItem.java
+++ src/main/java/io/github/hitsujirere/hnn_fabdirective/item/FabDirectiveItem.java
@@ -2,12 +2,18 @@ package io.github.hitsujirere.hnn_fabdirective.item;
 
 import dev.shadowsoffire.hostilenetworks.data.DataModel;
 import dev.shadowsoffire.hostilenetworks.tile.LootFabTileEntity;
+import dev.shadowsoffire.hostilenetworks.util.Color;
+import dev.shadowsoffire.placebo.PlaceboClient;
 import dev.shadowsoffire.placebo.reload.DynamicHolder;
 import io.github.hitsujirere.hnn_fabdirective.accessor.ISelectionsAccessor;
 import io.github.hitsujirere.hnn_fabdirective.util.SavedSelections;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.ObjectIterator;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
@@ -18,6 +24,7 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -51,7 +58,7 @@ public class FabDirectiveItem extends Item {
                 if (!selections.isEmpty()) {
                     ISelectionsAccessor selectionsAccessor = (ISelectionsAccessor) lootFab;
                     selectionsAccessor.setSelections(selections.getSelections());
-                    player.sendSystemMessage(Component.translatable("text.hnn_fabdirective.selections_applied", selectionsAccessor.getSelections().size(), stack.getDisplayName()).withStyle(ChatFormatting.GREEN));
+                    player.sendSystemMessage(Component.translatable("text.hnn_fabdirective.selections_applied", selectionsAccessor.getSelections().size(), stack.getDisplayName()).withStyle(style -> style.withColor(Color.LIME)));
                     return InteractionResult.SUCCESS;
                 }
             } else {
@@ -59,7 +66,7 @@ public class FabDirectiveItem extends Item {
                 Object2IntMap<DynamicHolder<DataModel>> fabSelections = selectionsAccessor.getSelections();
                 SavedSelections selections = new SavedSelections(fabSelections);
                 stack.getOrCreateTag().put("fab_selections", selections.write());
-                player.sendSystemMessage(Component.translatable("text.hnn_fabdirective.selections_copied", selectionsAccessor.getSelections().size(), stack.getDisplayName()).withStyle(ChatFormatting.GREEN));
+                player.sendSystemMessage(Component.translatable("text.hnn_fabdirective.selections_copied", selectionsAccessor.getSelections().size(), stack.getDisplayName()).withStyle(style -> style.withColor(Color.LIME)));
                 return InteractionResult.SUCCESS;
             }
 
@@ -80,5 +87,59 @@ public class FabDirectiveItem extends Item {
     public void appendHoverText(@NotNull ItemStack stack, Level level, List<Component> list, @NotNull TooltipFlag flag) {
         list.add(Component.translatable(this.getDescriptionId() + ".desc").withStyle(ChatFormatting.GRAY));
         list.add(Component.translatable(this.getDescriptionId() + ".desc2").withStyle(ChatFormatting.GRAY));
+        if (FMLEnvironment.dist.isClient()) {
+            ClientAccess.appendHoverText(stack, level, list, flag);
+        }
     }
+
+    public static class ClientAccess {
+
+        public static void appendHoverText(ItemStack stack, Level level, List<Component> list, TooltipFlag flag) {
+            SavedSelections selections = SavedSelections.read(stack.getTagElement("fab_selections"));
+            if (selections.isEmpty()) {
+                return;
+            }
+
+            list.add(CommonComponents.SPACE);
+
+            Object2IntMap<DynamicHolder<DataModel>> fabSelections = selections.getSelections();
+            int selIdx = selections.isEmpty() ? 0 : (int) ((PlaceboClient.ticks / 40) % selections.size());
+
+            ObjectIterator<Object2IntMap.Entry<DynamicHolder<DataModel>>> it = fabSelections.object2IntEntrySet().iterator();
+            it.skip(selIdx);
+
+            Object2IntMap.Entry<DynamicHolder<DataModel>> entry = it.next();
+            DynamicHolder<DataModel> holder = entry.getKey();
+            int index = entry.getIntValue();
+            DataModel model = holder.isBound() ? holder.get() : null;
+            ItemStack drop = model != null && index < model.fabDrops().size() ? model.fabDrops().get(index) : ItemStack.EMPTY;
+
+            MutableComponent comp = Component.translatable("text.hnn_fabdirective.stored_selection", selIdx + 1, selections.size()).withStyle(style -> style.withColor(Color.LIME));
+            if (Screen.hasShiftDown()) {
+                comp.append(CommonComponents.SPACE);
+                comp.append(Component.translatable("text.hnn_fabdirective.scroll").withStyle(style -> style.withColor(ChatFormatting.DARK_GRAY).withUnderlined(false)));
+            } else {
+                comp.append(CommonComponents.SPACE);
+                comp.append(Component.translatable("text.hnn_fabdirective.shift").withStyle(style -> style.withColor(ChatFormatting.DARK_GRAY).withUnderlined(false)));
+            }
+
+            list.add(comp);
+
+            if (model != null) {
+                if (!drop.isEmpty()) {
+                    Component input = model.getPredictionDrop().getHoverName();
+                    Component output = drop.getHoverName();
+                    comp = Component.translatable("text.hnn_fabdirective.selection", input, drop.getCount(), output).withStyle(style -> style.withColor(Color.LIME));
+                    list.add(comp);
+                } else {
+                    comp = Component.translatable("text.hnn_fabdirective.invalid_selection.index", index).withStyle(style -> style.withColor(ChatFormatting.RED));
+                    list.add(comp);
+                }
+            } else {
+                comp = Component.translatable("text.hnn_fabdirective.invalid_selection.model", holder.getId()).withStyle(style -> style.withColor(ChatFormatting.RED));
+                list.add(comp);
+            }
+        }
+    }
+
 }
